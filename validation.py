0e8ffaf2929cbdd91bc46d2157624b4017dcb5aa
Vishal Racheti <vishal.racheti6080@gmail.com>
2025-09-26 00:10:02 +0530

    validation changes

--- validation.py
+++ validation.py
@@ -1,6 +1,7 @@
 #valid user input
 #username is no more than 12 characters
 #username must not conatin digits
+#username must not contain any spaces
 
 
 username = input("Enter the username: ")
@@ -8,8 +9,13 @@ username = input("Enter the username: ")
 if len(username) > 12:
     print("Username must be no more than 12 characters.")
 
+elif ' ' in username:
+    print("Username must not contain spaces.")
+
 elif any(char.isdigit() for char in username):
     print("Username must not contain digits.")
 
 else:
     print(f"Username is valid. {username}")
+
+
